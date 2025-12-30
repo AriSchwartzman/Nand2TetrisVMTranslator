98217d30f4d6f9a935313be73f18edd370bcc7e4
AriSchwartzman <79026307+AriSchwartzman@users.noreply.github.com>
2025-12-30 02:24:59 -0500

    Updated standards for label names Added branching features (tested) Can work with files and directories Return command implemented (not tested) TODO: Call and Function commands

--- AssemblyGenerator.java
+++ AssemblyGenerator.java
@@ -1,16 +1,32 @@
+import java.util.Stack;
+
 public class AssemblyGenerator {
+
+    private Stack<String> functionStack = new Stack<>();
+
+    public AssemblyGenerator() {
+        this.functionStack.push(null);
+    }
 
     public String generateAssembly(String line, int counter, String baseFileName) {
         String[] splitCommand = line.split(" ");
-        if (line.contains("push") || line.contains("pop")) {
-            return stackCommand(line, splitCommand[0], splitCommand[1], splitCommand[2], baseFileName, counter);
+        if (splitCommand[0].equals("push") || splitCommand[0].equals("pop")) {
+            return stackCommand(line, splitCommand[0], splitCommand[1], splitCommand[2], baseFileName,
+                    functionStack.peek(), counter);
+        } else if (splitCommand.length == 1 && !line.equals("return")) {
+            return logicCommand(line, splitCommand[0], baseFileName, functionStack.peek(), counter);
+        } else if (splitCommand[0].equals("label") || splitCommand[0].contains("goto")) {
+            return branchingCommand(line, splitCommand[0], splitCommand[1], baseFileName, functionStack.peek(),
+                    counter);
+        } else if (splitCommand[0].equals("return")) {
+            return returnCommand();
         } else {
-            return logicCommand(line, splitCommand[0], counter);
+            return functionCommand(line, splitCommand[0], splitCommand[1], splitCommand[2], baseFileName, counter);
         }
     }
 
-
-    public String stackCommand(String line, String command, String segment, String data, String baseFileName, int counter) {
+    private String stackCommand(String line, String command, String segment, String data, String baseFileName,
+            String functionName, int counter) {
         String assembly;
         if (command.equals("push")) {
             switch (segment) {
@@ -43,7 +59,8 @@ public class AssemblyGenerator {
                     break;
 
                 case "pointer":
-                    assembly = "// " + line + "\n@%1$s\nD=A\n@POINTER%3$d\nD;JEQ\n@THAT\nD=A\n@ADDSTACK%3$d\n0;JMP\n(POINTER%3$d)\n@THIS\nD=A\n(ADDSTACK%3$d)\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+                    assembly = "// " + line
+                            + "\n@%1$s\nD=A\n@POINTER%3$d\nD;JEQ\n@THAT\nD=A\n@ADDSTACK%3$d\n0;JMP\n(POINTER%3$d)\n@THIS\nD=A\n(ADDSTACK%3$d)\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
                     break;
 
                 default:
@@ -53,11 +70,13 @@ public class AssemblyGenerator {
         } else {
             switch (segment) {
                 case "local":
-                    assembly = "// " + line + "\n@LCL\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+                    assembly = "// " + line
+                            + "\n@LCL\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
                     break;
 
                 case "argument":
-                    assembly = "// " + line + "\n@ARG\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+                    assembly = "// " + line
+                            + "\n@ARG\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
                     break;
 
                 case "static":
@@ -68,11 +87,13 @@ public class AssemblyGenerator {
                     throw new RuntimeException("error: cannot pop to constant segment");
 
                 case "this":
-                    assembly = "// " + line + "\n@THIS\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+                    assembly = "// " + line
+                            + "\n@THIS\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
                     break;
 
                 case "that":
-                    assembly = "// " + line + "\n@THAT\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+                    assembly = "// " + line
+                            + "\n@THAT\nD=M\n@%1$s\nD=D+A\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
                     break;
 
                 case "temp":
@@ -80,20 +101,21 @@ public class AssemblyGenerator {
                     break;
 
                 case "pointer":
-                    assembly = "// " + line + "\n@%1$s\nD=A\n@POINTER%3$d\nD;JEQ\n@THAT\nD=A\n@POPSTACK%3$d\n0;JMP\n(POINTER%3$d)\n@THIS\nD=A\n(POPSTACK%3$d)\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
+                    assembly = "// " + line
+                            + "\n@%1$s\nD=A\n@%2$s.%4$s$POINTER%3$d\nD;JEQ\n@THAT\nD=A\n@%2$s.%4$s$POPSTACK%3$d\n0;JMP\n(%2$s.%4$s$POINTER%3$d)\n@THIS\nD=A\n(%2$s.%4$s$POPSTACK%3$d)\n@R13\nM=D\n@SP\nAM=M-1\nD=M\n@R13\nA=M\nM=D\n";
                     break;
 
                 default:
-                    assembly = "";
+                    assembly = "// " + line + "\n";
                     break;
             }
         }
-        assembly = String.format(assembly, data, baseFileName, counter);
+        assembly = String.format(assembly, data, baseFileName, counter,
+                functionName == null ? baseFileName : functionName);
         return assembly + "\n";
     }
 
-
-    public String logicCommand(String line, String command, int counter) {
+    private String logicCommand(String line, String command, String baseFileName, String functionName, int counter) {
         String assembly;
 
         switch (command) {
@@ -110,18 +132,24 @@ public class AssemblyGenerator {
                 break;
 
             case "eq":
-                assembly = "// " + line + "\n@SP\nA=M-1\nD=M\nM=0\nA=A-1\nD=M-D\n@EQUAL%1$d\nD;JEQ\nD=0\n@SET%1$d\n0;JMP\n(EQUAL%1$d)\nD=-1\n(SET%1$d)\n@SP\nA=M-1\nA=A-1\nM=D\n@SP\nM=M-1\n";
-                assembly = String.format(assembly, counter);
+                assembly = "// " + line
+                        + "\n@SP\nA=M-1\nD=M\nM=0\nA=A-1\nD=M-D\n@%2$s.%3$s$EQUAL%1$d\nD;JEQ\nD=0\n@%2$s.%3$s$SET%1$d\n0;JMP\n(%2$s.%3$s$EQUAL%1$d)\nD=-1\n(%2$s.%3$s$SET%1$d)\n@SP\nA=M-1\nA=A-1\nM=D\n@SP\nM=M-1\n";
+                assembly = String.format(assembly, counter, baseFileName,
+                        functionName == null ? baseFileName : functionName);
                 break;
 
             case "gt":
-                assembly = "// " + line + "\n@SP\nA=M-1\nD=M\nM=0\nA=A-1\nD=M-D\n@EQUAL%1$d\nD;JGT\nD=0\n@SET%1$d\n0;JMP\n(EQUAL%1$d)\nD=-1\n(SET%1$d)\n@SP\nA=M-1\nA=A-1\nM=D\n@SP\nM=M-1\n";
-                assembly = String.format(assembly, counter);
+                assembly = "// " + line
+                        + "\n@SP\nA=M-1\nD=M\nM=0\nA=A-1\nD=M-D\n@%2$s.%3$s$EQUAL%1$d\nD;JGT\nD=0\n@%2$s.%3$s$SET%1$d\n0;JMP\n(%2$s.%3$s$EQUAL%1$d)\nD=-1\n(%2$s.%3$s$SET%1$d)\n@SP\nA=M-1\nA=A-1\nM=D\n@SP\nM=M-1\n";
+                assembly = String.format(assembly, counter, baseFileName,
+                        functionName == null ? baseFileName : functionName);
                 break;
 
             case "lt":
-                assembly = "// " + line + "\n@SP\nA=M-1\nD=M\nM=0\nA=A-1\nD=M-D\n@EQUAL%1$d\nD;JLT\nD=0\n@SET%1$d\n0;JMP\n(EQUAL%1$d)\nD=-1\n(SET%1$d)\n@SP\nA=M-1\nA=A-1\nM=D\n@SP\nM=M-1\n";
-                assembly = String.format(assembly, counter);
+                assembly = "// " + line
+                        + "\n@SP\nA=M-1\nD=M\nM=0\nA=A-1\nD=M-D\n@%2$s.%3$s$EQUAL%1$d\nD;JLT\nD=0\n@%2$s.%3$s$SET%1$d\n0;JMP\n(%2$s.%3$s$EQUAL%1$d)\nD=-1\n(%2$s.%3$s$SET%1$d)\n@SP\nA=M-1\nA=A-1\nM=D\n@SP\nM=M-1\n";
+                assembly = String.format(assembly, counter, baseFileName,
+                        functionName == null ? baseFileName : functionName);
                 break;
 
             case "and":
@@ -135,12 +163,61 @@ public class AssemblyGenerator {
             case "not":
                 assembly = "// " + line + "\n@SP\nA=M-1\nM=!M\n";
                 break;
-        
+
             default:
-                assembly = "";
+                assembly = "// " + line + "\n";
                 break;
         }
-
         return assembly + "\n";
+    }
+
+    private String branchingCommand(String line, String command, String data, String baseFileName, String functionName,
+            int counter) {
+        String assembly;
+
+        switch (command) {
+            case "label":
+                if (functionName == null) {
+                    assembly = "// " + line + "\n(" + baseFileName + "." + baseFileName + "$" + data + ")\n";
+                    break;
+                }
+                assembly = "// " + line + "\n(" + baseFileName + "." + functionName + "$" + data + ")\n";
+                break;
+
+            case "goto":
+                if (functionName == null) {
+                    assembly = "// " + line + "\n@" + baseFileName + "." + baseFileName + "$" + data + "\n0;JMP\n";
+                    break;
+                }
+                assembly = "// " + line + "\n@" + baseFileName + "." + functionName + "$" + data + "\n0;JMP\n";
+                break;
+
+            case "if-goto":
+                if (functionName == null) {
+                    assembly = "// " + line + "\n@SP\nAM=M-1\nD=M\n@" + baseFileName + "." + baseFileName + "$" + data
+                            + "\nD;JGT\nM=M+1\n";
+                    break;
+                }
+                assembly = "// " + line + "\n@SP\nAD=M\n@" + baseFileName + "." + functionName + "$" + data
+                        + "\nD;JGT\n";
+                break;
+
+            default:
+                assembly = "// " + line + "\n";
+                break;
+        }
+        return assembly + "\n";
+    }
+
+    private String returnCommand() {
+        return "// return\n@SP\nD=M\n@ARG\nA=M\nM=D\n@ARG\nD=M\n@SP\nM=D+1\n@LCL\nD=M\n@R14\nAM=D-1\nD=M\n@THAT\nM=D\n@R14\nAM=M-1\nD=M\n@THIS\nM=D\n@R14\nAM=M-1\nD=M\n@ARG\nM=D\n@R14\nAM=M-1\nD=M\n@LCL\nM=D\n@R14\nA=M-1\nA=M\n0;JMP";
+    }
+
+    private String functionCommand(String line, String command, String newFunctionName, String data,
+            String baseFileName,
+            int counter) {
+        String assembly;
+        assembly = "// " + line + "\n";
+        return assembly;
     }
 }
--- VMTranslator.java
+++ VMTranslator.java
@@ -1,19 +1,34 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 public class VMTranslator {
     public static void main(String[] args) {
         AssemblyGenerator generator = new AssemblyGenerator();
         Parser parser = new Parser(generator);
-        String fileName = args[0];
-        File vmFile = new File(fileName);
-        String baseFileName = fileName.substring(0, fileName.length() - 2);
-        String assemblyFileName = baseFileName + "asm";
+        File[] files;
         String assemblyCode = "";
+        String assemblyFilePath;
+
+        if (args[0].endsWith(".vm")) {
+            files = new File[1];
+            files[0] = new File(args[0]);
+            assemblyFilePath = args[0].substring(0, args[0].length() - 2) + "asm";
+        } else {
+            File directory = new File(args[0]);
+
+            files = directory.listFiles(new FilenameFilter() {
+                public boolean accept(File f, String fileType) {
+                    return fileType.endsWith(".vm");}
+            });
+            System.out.println(files.length);
+            assemblyFilePath = args[0] + ".asm";
+        }
+
 
         try {
-            File myObj = new File(assemblyFileName); // Create File object
+            File myObj = new File(assemblyFilePath); // Create File object
             if (myObj.createNewFile()) { // Try to create the file
                 System.out.println("File created: " + myObj.getName());
             } else {
@@ -24,12 +39,17 @@ public class VMTranslator {
             e.printStackTrace(); // Print error details
         }
 
-        String cleanVMCode = parser.prettify(vmFile);
+        for (File vmFile : files) {
+            String fileName = vmFile.getName();
+            String baseFileName = fileName.substring(0, fileName.length() - 3);
+            System.out.println(baseFileName);
+            String cleanVMCode = parser.prettify(vmFile);
+            assemblyCode += parser.generateAssembly(cleanVMCode, baseFileName);
+        }
 
-        assemblyCode = parser.generateAssembly(cleanVMCode, baseFileName);
 
         try {
-            FileWriter myWriter = new FileWriter(assemblyFileName);
+            FileWriter myWriter = new FileWriter(assemblyFilePath);
             myWriter.write(assemblyCode);
             myWriter.close(); // must close manually
             System.out.println("Successfully wrote to the file.");
